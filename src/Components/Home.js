2c37ff3dd3ed54160bac016a0942c38cacbdd692
Amit <amitsaliap@gmail.com>
2022-02-14 23:15:16 +0530

    properties which are used in project are commented out are they are used in project to identify them why we actually wrote them

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import LeftBar from './LeftBar';
-import Navbar from './Navbar';
-import VedioContainer from './VedioContainer';
-//import Login from './Login'
+//import LeftBar from './LeftBar';
+//import Navbar from './Navbar';
+//import VedioContainer from './VedioContainer';
+import Login from './Login'
 // Vedio container made by Yadhnesh...
 export default function Home() {
   return (
     <>
-      <Navbar/>
-      <LeftBar/>
-      <VedioContainer/>
+      <Login/>
     </>
   );
 }
